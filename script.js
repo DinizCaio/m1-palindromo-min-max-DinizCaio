98a5a30e50895a2e97ed7a3e995b776855c82b5e
Caio Diniz <dinizcaio97@gmail.com>
2024-09-20 17:44:04 -0300

    Função verificadora de palíndromo
    Concluída a primeira função do projeto

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  let strFormat = str.toLowerCase();
+  let arr = [];
+  for (let i = 0; i < strFormat.length; i++) {
+    if (strFormat[i] != " ") {
+      arr.push(strFormat[i]);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != arr[arr.length - 1]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
-
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+function arrayMaxMin(arr) {}
